46d5a8bbc29f513d4adeebdaeb6972290134cb3e
Vinícius <Vincius@VINICIUS-NOTE2>
2019-05-15 00:13:36 -0300

    Scanner para capturar entrada na linha de comando

--- src/main/java/br/com/integracao/examescliente/ExamesClienteApplication.java
+++ src/main/java/br/com/integracao/examescliente/ExamesClienteApplication.java
@@ -1,11 +1,14 @@
 package br.com.integracao.examescliente;
 
+import java.util.Scanner;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 import br.com.integracao.examescliente.soap.BuscaExameRequisicao;
+import br.com.integracao.examescliente.soap.Exame;
 import br.com.integracao.examescliente.soap.ExameResposta;
 
 @SpringBootApplication
@@ -18,17 +21,27 @@ public class ExamesClienteApplication {
 	@Bean
 	public CommandLineRunner lookup(SOAPConnector soapConnector) {
 		return args -> {
-			long id = 1l;
-			BuscaExameRequisicao request = new BuscaExameRequisicao();
-			request.setId(id);
-			request.setCodigo("teste");
-			ExameResposta response =(ExameResposta) soapConnector.callWebService("http://localhost:8080/service/integracao-exames", request);
-			System.out.println("Got Response As below ========= : ");
-			System.out.println("Id : "+response.getExame().getId());
-			System.out.println("Código : "+response.getExame().getCodigo());
-			System.out.println("Nome : "+response.getExame().getNome());
-			System.out.println("Descrição : "+response.getExame().getDescricao());
-
+		    try (Scanner scanner = new Scanner(System.in)){;
+			    System.out.print("Digite o id do exame: ");
+			    long id = scanner.nextLong();
+			    
+				BuscaExameRequisicao request = new BuscaExameRequisicao();
+				request.setId(id);
+	
+				ExameResposta response =(ExameResposta) soapConnector.callWebService("http://localhost:8080/service/integracao-exames", request);
+				Exame exame = response.getExame();
+				
+				if(exame != null) {
+					System.out.println("Exame encontrado: ========= : ");
+					System.out.println("Id : "+response.getExame().getId());
+					System.out.println("Código : "+response.getExame().getCodigo());
+					System.out.println("Nome : "+response.getExame().getNome());
+					System.out.println("Descrição : "+response.getExame().getDescricao());
+				}else {
+					System.out.println("Nenhhum exame encontrado! ");
+				}
+		    }
+			
 		};
 	}
 
